b6ce95762b610e951d7fbd4df3d25418b41dfead
Marcos García <marcos.g.ciruelos@gmail.com>
2015-12-13 18:50:06 +0100

    New Exercises added.

--- haskell/learnHaskell/SintaxisFunciones.hs
+++ haskell/learnHaskell/SintaxisFunciones.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 lucky :: (Integral a) => a -> String
 lucky 7 = "¡El siete de la suerte!"
 lucky x = "Lo siento, ¡no es tu día de suerte!"
@@ -35,7 +37,23 @@ tell (x:[])   = "La lista tiene un elemento: " ++ show x
 tell (x:y:[]) = "La lista tiene dos elementos: " ++ show x ++ " y " ++ show y
 tell (x:y:_)  = "La lista es larga. Los primeros dos elementos son: " ++ show x ++ " y " ++ show y
 
---deleteDuplicateZero [] = []
---deleteDuplicateZero x : [] = x
---deleteDuplicateZero x : y : _ = 
---deleteDuplicateZero
+
+deleteDuplicateZero :: (Integral a) => [a] -> [a]
+deleteDuplicateZero [] = []
+deleteDuplicateZero (x:[]) = x:[]
+deleteDuplicateZero (x:y:xs)
+    | x == y    = deleteDuplicateZero (y:xs)
+    | otherwise = x : deleteDuplicateZero (y:xs)
+
+deleteDuplicate [] = []
+deleteDuplicate (x:[]) = x:[]
+deleteDuplicate (x:xs)
+    | elem x xs = deleteDuplicate xs
+    | otherwise = x : deleteDuplicate xs
+
+removeDuplicateAndOrder xs = sort $ deleteDuplicate xs
+
+scanSum ::  [Int] -> [Int]
+scanSum [] = []
+scanSum (a:[]) = a:[]
+scanSum (a:b:as) = a : scanSum ((a+b):as)
