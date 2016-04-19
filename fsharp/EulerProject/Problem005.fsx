9ddadc5dc9d9459e9d27afe108f2fa9078f823e8
Marcos García <marcos.g.ciruelos@gmail.com>
2016-04-19 22:30:10 +0200

    Trying resolve Euler 5.

--- fsharp/EulerProject/Problem005.fsx
+++ fsharp/EulerProject/Problem005.fsx
@@ -11,9 +11,22 @@ let rec generatePrimeSeq list = function
 let maxMult = generatePrimeSeq [] [2..20] |> List.fold (fun acc x -> acc * x) 1
 printfn "%A" maxMult
 
-let seq = Seq.unfold (fun x -> Some(x, x+ 1)) 0
-let filterSeq = seq |> Seq.find (fun x -> x = maxMult)
+let odds = 1::2:: [3..5..10]
+let divisibleBy x y = if (x % y = 0) then true else false
+let divisibleByOdds index = odds |> List.filter (fun x -> divisibleBy index x) 
+let seq = Seq.unfold (fun x -> Some(x, x+ 1)) 11
+let filterSeq = seq |> Seq.find (fun x -> (List.length (divisibleByOdds x)) = 6)
 printfn "%A" filterSeq
 
 //://stefanoricciardi.com/2010/08/12/project-euler-problem-5-in-f/
 // http://theburningmonk.com/2010/09/project-euler-problem-5-solution/
+
+let isEvenlyDivided(n, m) = n % m = 0
+let isEvenlyDividedByAll(n, numbers) = numbers |> Seq.forall (fun x -> isEvenlyDivided(n, x))
+
+ let findSmallestCommonMultiple(numbers) =
+         let max = Array.max(numbers)
+             Seq.unfold (fun x -> Some(x, x + 1)) max
+                 |> Seq.filter (fun x -> isEvenlyDividedByAll(x, numbers))
+                     |> Seq.head
+                      
